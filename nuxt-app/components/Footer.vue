dab4aa8defad96d30cf8f569003d5d3f288792ce
wngks8321 <wngks8321@gmail.com>
2022-06-19 15:25:36 +0900

    header, footer컴포넌트 default 레이아웃 추가

---
+++ nuxt-app/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <h2>Footer</h2>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -2,5 +2,14 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  baseUrl: "/",
+  app: {
+    head: {
+      "charset": "utf-8",
+      "title": "Nuxt3 테스트",
+      "style": [
+        { type: 'text/css', src: 'assets/styles/reset.css' }
+      ]
+    }
+  }
 })
